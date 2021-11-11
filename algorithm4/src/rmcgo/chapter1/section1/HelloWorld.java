06972c0c74490b21e480b5758332567c436762b9
rmcgo <rmc7691@gmail.com>
2021-11-11 08:19:19 +0800

    mldn chapter5 one

---
+++ algorithm4/src/rmcgo/chapter1/section1/HelloWorld.java
@@ -0,0 +1,11 @@
+package rmcgo.chapter1.section1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("加油");
+		}
+	}
+}
---
+++ mldn/src/com/rmcgo/chapter5/one/AddressTest.java
@@ -0,0 +1,8 @@
+package com.rmcgo.chapter5.one;
+
+public class AddressTest {
+	public static void main(String[] args) {
+		Address myAddress = new Address("China", "Shanghai", "Shanghai", "Hongling line", "200000");
+		System.out.println(myAddress);
+	}
+}
